244b92c3f4f1cc974a298e015744e6197c4c2c84
Kathleen Qian <kathleenqsy@outlook.com>
2020-12-14 08:16:43 +0000

    Get permanently stored comments and show them all

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,10 +14,12 @@
 
 package com.google.sps.servlets;
 
-import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,10 +31,18 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content and handles data. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private List<String> commentsStorage = new ArrayList<>();
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<String> commentsStorage = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      String contents = (String) entity.getProperty("contents");
+      commentsStorage.add(contents);
+    }
+
     String json = new Gson().toJson(commentsStorage);
     response.setContentType("text/html");
     response.getWriter().println(json);
