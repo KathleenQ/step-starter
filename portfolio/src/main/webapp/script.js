6e7a0ea379d3ba9d9f99ae5f5a2dbf6053adec53
Kathleen Qian <sykq@google.com>
2020-12-23 02:34:04 +0000

    Basic maps function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -53,3 +53,13 @@ function fetchBlobstoreUrlAndShowForm() {
         messageForm.classList.remove('hidden');
       });
 }
+
+/** Adds a map to the page. */
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
