bb993a4b2b1a840b6903f90aa1618e6a3f4b3c25
Kathleen Qian <sykq@google.com>
2020-12-23 03:01:06 +0000

    Adjust the location

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -58,8 +58,8 @@ function fetchBlobstoreUrlAndShowForm() {
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -35.280, lng: 149.119},
+    zoom: 15,
   });
 }
